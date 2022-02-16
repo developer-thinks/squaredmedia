12b8142c9ac973ccecb2fd3389732a40bf9ee79d
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-02-17 00:22:27 +0530

    receiving the data from postman and saving to database with securing password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,16 +1,28 @@
 const router = require('express').Router()
 const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
 //Register new user :
-router.get("/register", async (req,res)=>{
-    const user = await new User({
-        username : "samsquare",
-        email : "sam@gmail.com",
-        password : "sam@123",
-    })
+router.post("/register", async (req,res)=>{
+    
+    try {
+        // generating secure password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-    await user.save();
-    res.send("saved")
+        //creating new user with secure password
+        const newUser = await new User({
+            username : req.body.username,
+            email : req.body.email,
+            password : hashedPassword
+        })
+
+        //saving user to database
+        const user = await newUser.save();
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(422).json({error})
+    }
 });
 
 module.exports = router
