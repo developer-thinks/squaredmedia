f95050b583ac77f4e4cc9c1a9862e33427d59476
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-02-16 23:38:42 +0530

    the way to use the express router for auth (similar to prev)

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 
 const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 
 dotenv.config()
@@ -21,6 +22,7 @@ app.use(helmet())
 app.use(morgan("common"))
 
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute)
 
 
 app.listen(5000,()=>{
