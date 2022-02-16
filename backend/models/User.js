c31f2f6ecb6122255d0c4b5875cc6de521587aba
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-02-17 01:35:53 +0530

    update the user and delete the user

--- backend/models/User.js
+++ backend/models/User.js
@@ -39,6 +39,25 @@ const UserSchema = new mongoose.Schema({
     isAdmin : {
         type : Boolean,
         default : false
+    },
+    description: {
+      type: String,
+      max: 50,
+      default:""
+    },
+    city: {
+      type: String,
+      max: 50,
+      default:""
+    },
+    from: {
+      type: String,
+      max: 50,
+      default:""
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
     }
 },{
     timestamps : true
