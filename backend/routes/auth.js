5dfdf42b798e34230f37217c1c5908b909b83a50
sammed-sankonatti <sammed.sankonatti45@gmail.com>
2022-02-17 00:51:22 +0530

    this is the way of authentictaing the user with database

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -25,4 +25,20 @@ router.post("/register", async (req,res)=>{
     }
 });
 
+router.post('/login', async (req,res)=>{
+    try {
+        //checking if the user by her email(which we receive from postman)
+        const user = await User.findOne({email : req.body.email});
+        !user && res.status(404).json("user Not found")
+
+        //now if user present check the password is correct :
+        const validatePassword = await bcrypt.compare(req.body.password, user.password);
+        !validatePassword && res.status(400).json("wrong credentials")
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 module.exports = router
